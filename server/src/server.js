861a3a0e93b1dacb2133db5932aefe039f0c6411
Matt K <matthew.b.kurtti@gmail.com>
2025-02-16 21:24:07 +0900

    feat: added authentication routes to server
    also, added get deck ids of single user route and get all cards of single deck route

--- server/src/server.js
+++ server/src/server.js
@@ -1,26 +1,134 @@
 // imports
 const express = require("express");
 const knex = require("./knex");
+const cors = require("cors");
+const path = require("path");
+const session = require("express-session");
+const pgSession = require("connect-pg-simple")(session);
+const bcrypt = require("bcrypt");
 
 // variables
 const app = express();
 const port = process.env.PORT || 8080;
 
 // --------------- Middleware -------------
-// if (!process.env.NODE_ENV) {
-//   app.use(
-//     cors({
-//       origin: "http://localhost:5173",
-//       credentials: true,
-//     })
-//   );
-// } else {
-//   app.use("/", express.static(path.join(__dirname, "../../client/dist")));
-// }
+if (!process.env.NODE_ENV) {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+      credentials: true,
+    })
+  );
+} else {
+  app.use("/", express.static(path.join(__dirname, "../../client/dist")));
+}
 
 app.use(express.json());
 
+app.use(
+  session({
+    store: new pgSession({
+      conString:
+        process.env.DATABASE_URL ||
+        `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
+    }),
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: { secure: false },
+  })
+);
+
+// -------------- Authentication ---------------------------
+// it (should allow a new user to register)
+app.post("/api/auth/register", async (req, res) => {
+  try {
+    const { username, password, email } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const [newUser] = await knex("users")
+      .insert({
+        username,
+        password: hashedPassword,
+        email,
+      })
+      .returning(["id", "username", "email"]);
+
+    res
+      .status(201)
+      .json({ message: "User registered successfully", user: newUser });
+  } catch (error) {
+    console.error("Database connection error.", error);
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// it (should allow an existing user to login)
+app.post("/api/auth/login", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    const user = await knex("users").where({ username }).first();
+
+    if (!user) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+
+    req.session.userId = user.id;
+    req.session.username = user.username;
+
+    res.json({ message: "Login succesful" });
+  } catch (error) {
+    console.error("Database connection error.", error);
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Checks if a user is logged in
+// it (should fetch logged in users profile from database using the userId stored in the session)
+app.get("/api/auth/user", (req, res) => {
+  if (!req.session.userId) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+  knex("users")
+    .select("id", "username", "email")
+    .where({ id: req.session.userId })
+    .first()
+    .then((user) => {
+      if (!user) {
+        return res.status(404).json({ message: "User not found" });
+      }
+
+      res.json(user);
+    })
+    .catch((error) => {
+      console.error("Database connection error.", error);
+      res.status(500).json({ error: error.message });
+    });
+});
+
+// GET /api/auth/logout
+// it (should log the user out)
+app.get("/api/auth/logout", (req, res) => {
+  req.session.destroy((err) => {
+    if (err) {
+      return res.status(500).json({ error: "Failed to logout" });
+    }
+
+    res.json({ message: "Logout  successful" });
+  });
+});
+// ----------- Authentication (END) -----------
+
 // --------------- Routes -----------------------
+
+// cards
 
 // get all cards
 
@@ -29,6 +137,47 @@ app.get("/api/card", async (req, res) => {
     const card = await knex.select("*").from("card").limit(100);
 
     res.json({ card });
+  } catch (error) {
+    console.error("Database connection error:", error);
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// user
+
+// get all deck ids and names of a single user
+app.get("/api/deck/:userId", async (req, res) => {
+  try {
+    const userId = req.params.userId;
+    const userDecks = await knex
+      .select("id", "name")
+      .from("deck")
+      .where({ users_id: userId });
+
+    res.json({ userDecks });
+  } catch (error) {
+    console.error("Database connection error:", error);
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// get all cards of a single deck by deck id
+app.get("/api/card/:deckId", async (req, res) => {
+  try {
+    const deckId = req.params.deckId;
+    const allCardsIdsInDeck = await knex
+      .select("id")
+      .from("deck_card")
+      .where({ deck_id: deckId });
+    const allCards = [];
+    for (let i = 0; i < allCardsIdsInDeck.length; i++) {
+      const card = await knex
+        .select("front", "back")
+        .from("card")
+        .where({ id: allCardsIdsInDeck[i].id });
+      allCards.push(card[0]);
+    }
+    res.json({ allCards });
   } catch (error) {
     console.error("Database connection error:", error);
     res.status(500).json({ error: error.message });
