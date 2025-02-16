68926bf9d26639e3b5ac81f13b8ddef12ef13efb
Matt K <matthew.b.kurtti@gmail.com>
2025-02-16 16:57:20 +0900

    feat: make tailwind theme colors and init navbar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,10 @@
 import "./App.css";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <>
+      <NavBar />
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </>
   );
---
+++ client/src/components/NavBar.tsx
@@ -0,0 +1,12 @@
+function NavBar() {
+  return (
+    <>
+      <nav className="bg-secondary-accent h-15 flex justify-between items-center">
+        <h1 className="text-3xl font-bold">GoRaku</h1>
+        <h1 className="text-3xl">Logout</h1>
+      </nav>
+    </>
+  );
+}
+
+export default NavBar;
