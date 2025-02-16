ce6e21399a205bc7ee80966eb22878f44f75ab57
Matt K <matthew.b.kurtti@gmail.com>
2025-02-16 15:39:57 +0900

    feat: add get all cards route and use japanese in card seed

--- server/db/seeds/003-card.js
+++ server/db/seeds/003-card.js
@@ -5,14 +5,14 @@
 exports.seed = async function (knex) {
   await knex("card").insert([
     { id: 1, front: "質問 1", back: "answer 1" }, // come back and make sure these japanese characters show up right when pulled
-    { id: 2, front: "question 2", back: "answer 2" },
-    { id: 3, front: "question 3", back: "answer 3" },
-    { id: 4, front: "question 4", back: "answer 4" },
-    { id: 5, front: "question 5", back: "answer 5" },
-    { id: 6, front: "question 6", back: "answer 6" },
-    { id: 7, front: "question 7", back: "answer 7" },
-    { id: 8, front: "question 8", back: "answer 8" },
-    { id: 9, front: "question 9", back: "answer 9" },
-    { id: 10, front: "question 10", back: "answer 10" },
+    { id: 2, front: "質問 2", back: "answer 2" },
+    { id: 3, front: "質問 3", back: "answer 3" },
+    { id: 4, front: "質問 4", back: "answer 4" },
+    { id: 5, front: "質問 5", back: "answer 5" },
+    { id: 6, front: "質問 6", back: "answer 6" },
+    { id: 7, front: "質問 7", back: "answer 7" },
+    { id: 8, front: "質問 8", back: "answer 8" },
+    { id: 9, front: "質問 9", back: "answer 9" },
+    { id: 10, front: "質問 10", back: "answer 10" },
   ]);
 };
--- server/src/server.js
+++ server/src/server.js
@@ -1,7 +1,41 @@
+// imports
 const express = require("express");
+const knex = require("./knex");
 
+// variables
 const app = express();
 const port = process.env.PORT || 8080;
+
+// --------------- Middleware -------------
+// if (!process.env.NODE_ENV) {
+//   app.use(
+//     cors({
+//       origin: "http://localhost:5173",
+//       credentials: true,
+//     })
+//   );
+// } else {
+//   app.use("/", express.static(path.join(__dirname, "../../client/dist")));
+// }
+
+app.use(express.json());
+
+// --------------- Routes -----------------------
+
+// get all cards
+
+app.get("/api/card", async (req, res) => {
+  try {
+    const card = await knex.select("*").from("card").limit(100);
+
+    res.json({ card });
+  } catch (error) {
+    console.error("Database connection error:", error);
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// --------------- Start the server ---------------------
 
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
