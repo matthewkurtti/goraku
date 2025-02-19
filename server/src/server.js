54bc6f48b72011f995f8fc539ee761a9a39c9d7e
Matt K <matthew.b.kurtti@gmail.com>
2025-02-19 14:21:11 +0900

    first-deploy: define __dirname in server

--- server/src/server.js
+++ server/src/server.js
@@ -13,6 +13,10 @@ const pgSession = require("connect-pg-simple")(session);
 const bcrypt = require("bcrypt");
 // const bodyParser = require("body-parser");
 
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
+const __dirname = path.dirname(__filename); // get the name of the directory
+
 // google api imports
 import { createRequire } from "module";
 
